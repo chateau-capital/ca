e28670734d2e48b55f73fa0bf60af245fa37c9cd
0xKaso <kaso@desyn.io>
2024-01-31 18:02:59 +0800

    update vault pool with pause

--- contracts/VaultPool.sol
+++ contracts/VaultPool.sol
@@ -3,28 +3,46 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "./interface/IERC20Burnable.sol";
 import "./utils/NotAmerica.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 
-contract VaultPool is Ownable, NotAmerica {
+contract VaultPool is Ownable, NotAmerica,Pausable {
     IERC20 public issueToken;
-    IERC20 public redeemToekn;
+    IERC20Burnable public shareToekn;
     address public stakingPool;
 
     constructor(
         address _issueToken,
-        address _redeemToekn,
+        address _shareToekn,
         address _stakingPool
     ) Ownable(msg.sender) {
         issueToken = IERC20(_issueToken);
-        redeemToekn = IERC20(_redeemToekn);
+        shareToekn = IERC20Burnable(_shareToekn);
         stakingPool = _stakingPool;
     }
 
-    function reedem(uint256 amount) public {
-        uint redeemTotal = redeemToekn.totalSupply(); // share
-        uint issueTotal = issueToken.balanceOf(address(this)); // usdt
+    function reedem(uint256 amount) public whenNotPaused{
+        uint shareTotal = shareToekn.totalSupply();
+        uint issueTotal = issueToken.balanceOf(address(this));
+        uint withdrawAmount = (amount * issueTotal - 1) / (shareTotal + 1)  ;
 
-        uint redeemAmount = (redeemTotal * amount - 1) / (issueTotal + 1);
-        redeemToekn.transfer(msg.sender, redeemAmount);
+        require(withdrawAmount > 0 && issueTotal > 0, "withdrawAmount is zero");
+        shareToekn.transferFrom(msg.sender, address(this), amount);
+        shareToekn.burn(amount);
+        shareToekn.transfer(msg.sender, withdrawAmount);
+    }
+
+    function withdraw() public onlyOwner {
+        uint balance = issueToken.balanceOf(address(this));
+        issueToken.transfer(msg.sender, balance);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
     }
 }

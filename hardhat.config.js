47b10d73236feeb1fd82a22ffbb5d13e1fa18532
haeli05 <havoc795@hotmail.com>
2024-03-20 11:10:09 -0400

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 require("solidity-coverage");
 require("@nomiclabs/hardhat-solhint");
-
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -25,22 +25,12 @@ module.exports = {
     //   url: process.env.SEPOLIA_URL,
     //   accounts: [process.env.SEPOLIA_PRIVATE_KEY]
     // },
-    arbitrumOne:{
-      url: `https://1rpc.io/arb`,
-      accounts: [process.env.PRIVATE_KEY],
-      saveDeployments: true,
-      tags: ["arbitrum"],
-    },
-    neondevnet: {
-      url: "https://devnet.neonevm.org",
-      accounts: [process.env.PRIVATE_KEY],
-      chainId: 245022926
-    },
-    neonmainnet: {
-        url: "https://neon-proxy-mainnet.solana.p2p.org",
-        accounts: [process.env.PRIVATE_KEY],
-        chainId: 245022934
-    }
+    // arbitrumOne:{
+    //   url: `https://1rpc.io/arb`,
+    //   accounts: [process.env.PRIVATE_KEY],
+    //   saveDeployments: true,
+    //   tags: ["arbitrum"],
+    // }
     // arbitrumSepolia: {
     //   url: `${process.env.ARB_SEPOLIA_NODE}${process.env.ARB_SEPOLIA_ALCHEMY_KEY}`,
     //   accounts: [process.env.PRIVATE_KEY],
@@ -53,37 +43,22 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      neonevm: "test",
-      sepolia: process.env.ETHERSCAN_API_KEY || "",
-      etherscan: process.env.ETHERSCAN_API_KEY || "",
+      // neonevm: "test",
+      // sepolia: process.env.ETHERSCAN_API_KEY || "",
+      // etherscan: process.env.ETHERSCAN_API_KEY || "",
       arbitrumOne: process.env.ARBSCAN_APIKEY || "",
-      arbitrumSepolia: process.env.ARBSCAN_APIKEY || "",
+      // arbitrumSepolia: process.env.ARBSCAN_APIKEY || "",
     },
-    customChains: [
-      {
-        network: "arbitrumSepolia",
-        chainId: 421614,
-        urls: {
-          apiURL: "https://api-sepolia.arbiscan.io/api",
-          browserURL: "https://sepolia.arbiscan.io/",
-        },
-      },
-      {
-          network: "neonevm",
-          chainId: 245022926,
-          urls: {
-              apiURL: "https://devnet-api.neonscan.org/hardhat/verify",
-              browserURL: "https://devnet.neonscan.org"
-          }
-      },
-      {
-          network: "neonevm",
-          chainId: 245022934,
-          urls: {
-              apiURL: "https://api.neonscan.org/hardhat/verify",
-              browserURL: "https://neonscan.org"
-          }
-      }
-    ],
+    // customChains: [
+    //   // {
+    //   //   network: "arbitrumSepolia",
+    //   //   chainId: 421614,
+    //   //   urls: {
+    //   //     apiURL: "https://api-sepolia.arbiscan.io/api",
+    //   //     browserURL: "https://sepolia.arbiscan.io/",
+    //   //   },
+    //   // }
+    // ],
   },
 };
+

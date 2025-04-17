4fb1435e73379652cf1334c27214dc17e4b395c0
haeli05 <havoc795@hotmail.com>
2025-04-17 17:29:32 -0400

    usdc swap

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,29 +9,23 @@ const hre = require("hardhat");
 async function main() {
   // const USDTCoin = await hre.ethers.deployContract("USDT");
   // const usdtCoin = await USDTCoin.waitForDeployment();
+  // USDC on Arbitrum 0xaf88d065e77c8cC2239327C5EDb3A432268e5831
 
-  const usdtCoin = await hre.ethers.getContractAt("USDT","0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9");
+  // const usdtCoin = await hre.ethers.getContractAt("USDT","0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9");
 
-  // const QuadReaderUtils = await hre.ethers.deployContract("QuadReaderUtils");
-  // const quadReaderUtils = await QuadReaderUtils.waitForDeployment();
 
-  const Factory = await hre.ethers.deployContract("Factory", {
-    libraries: {
-      QuadReaderUtils: "0x122E7d91d384619FF9698F14ebC418DF697a1678",
-    },
-  });
+  const usdcCoin = await hre.ethers.getContractAt("USDC","0xaf88d065e77c8cC2239327C5EDb3A432268e5831");
+
+  const Factory = await hre.ethers.deployContract("Factory");
   const factory = await Factory.waitForDeployment();
 
-  const Fund = await factory.newFund.staticCall("RWA","RWA",usdtCoin.target);
+  const Fund = await factory.newFund.staticCall("FundName","fTicker",usdcCoin.target);
 
-
-  await factory.newFund("Chateau Alternative Debt","CHAD.D", usdtCoin.target);
-
-  
+  await factory.newFund("Chateau Alternative Debt","ch.AD", usdcCoin.target);
 
   console.table({
     shareCoin: Fund[0],
-    usdtCoin: usdtCoin.target,
+    usdtCoin: usdcCoin.target,
     stakingPool: Fund[1],
     vaultPool: Fund[2],
     factory: factory.target,
